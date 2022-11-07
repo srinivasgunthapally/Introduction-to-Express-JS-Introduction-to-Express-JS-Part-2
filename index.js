04b61e07c814ed6d420a8fabe03e6715663d0e40
srinivas <srinivas@example.com>
2022-11-07 05:55:41 +0000

    Introduction to Express JS > Introduction to Express JS | Part 2

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+const dbPath = path.join(__dirname, "goodreads.db");
+let db = null;
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("sever running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    SELECT * FROM book ORDER BY book_id ;
+    `;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
